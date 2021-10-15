d1eb34fac5fb282a6926616be833a6469b0b5d6c
atnishat <nishat.mu47@gmail.com>
2021-10-15 20:40:59 +0600

    first commit

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,41 @@
+<<!DOCTYPE html>
+<html>
+
+  <head>
+   <title></title>
+  
+  </head>
+  <body>
+    
+
+  <x-app-layout>
+   
+   </x-app-layout>
+   
+   
+   <!DOCTYPE html>
+   <html lang="en">
+   
+     <head>
+      
+     @include("admin.admincss")
+   
+     </head>
+     <body>
+     <div class="container-scroller">
+       @include("admin.navbar")
+
+       <h1>USERRRRR </h1>
+
+</div>
+       @include("admin.adminscript")
+       
+     </body>
+   </html>
+
+
+
+
+
+  </body>
+</html>
